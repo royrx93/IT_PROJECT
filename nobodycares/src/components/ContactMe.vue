98c0f684061d6a7d32dbc5880481d39ecfcc3ddd
royrx93 <royfmttm@gmail.com>
2020-10-03 01:59:42 +0800

    connect contactMe successfully but, still need more beautify

--- nobodycares/src/components/ContactMe.vue
+++ nobodycares/src/components/ContactMe.vue
@@ -1,60 +1,96 @@
 <template>
-    <div>
-        <H1>Register</H1>
-        
-        <input
-            type="firstname"
-            name="firstname"
-            v-model="firstname"
-            placeholder="firstname">
-        <br>
-        <input
-            type="lastname"
-            name="lastname"
-            v-model="lastname"
-            placeholder="lastname">
-        <br>
-        <input
-            type="email_address"
-            name="email_address"
-            v-model="emailaddress"
-            placeholder="email">
-        <br>
-        <input
-            type="content"
-            name="content"
-            v-model="content"
-            placeholder="content">
-        <br>        
-        <button @click="ContactMe">Submit</button>
+  <div class="contact">
+    <H1>Contact Me</H1>
+    <div class="input-div1">
+      <label for="name">Your name:</label>
+      <input type="text" name="name" v-model.trim="name">
     </div>
+    <div class="input-div2">
+      <label for="email">Your email: </label>
+      <input type="text" name="email" v-model.trim="email">
+    </div>
+
+    <div class="input-div3">
+      <label for="message">Message: </label>
+      <input type="text" name="message" v-model.trim="message">
+    </div>
+
+    <button @click="submit">Submit</button>
+  </div>
 </template>
 
 <script>
-    import ContactService from "../services/ContactService";
-    export default {
-        data(){
-            return{
-                firstname:'',
-                lastname:'',
-                email_address:'',
-                content:''
-            }
-        },
-        methods:{
-            async contactMe(){
-                const response = await ContactService.contact({
-                    firstname: this.firstname,
-                    lastname: this.lastname,
-                    email_address:this.email_address,
-                    content: this.content
-                })
-                console.log(response.data())
-            }
-        }
-    }
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3000/'
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    };
+  },
+  methods: {
+    submit() {
+      if (!this.name) {
+        alert('Please enter your name');
+        return;
+      } else if (!this.email) {
+        alert('Please enter your email address');
+        return;
+      } else {
+        let formData = {
+          name: this.name,
+          email: this.email,
+          message: this.message,
+        };
+        axios({
+          method: 'post',
+          url: '/mail',
+          data: formData,
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
+    .contact{
+    width:99.15%;
+    height:100%;
+    z-index:-1;
+    position: absolute;
+    }
+    H1{
+    color: #F0F0F0;
+    padding: 60px;
+    font-size:3em;
+    }
+    input-div1{
+        margin: 8px;
+        height:60px;
+        width: 600px;
+        font-size: 1.2em;
+    }
+    input-div2{
+        margin: 8px;
+        height:60px;
+        width: 600px;
+        font-size: 1.2em;
+    }
+    input-div3{
+        margin: 80px;
+        height:120px;
+        width: 1200px;
+        font-size: 1.2em;
+        
+    }
+    button{
+        margin: 60px;
+        height: 60px;
+        width: 300px;
+        font-size: 1.2em;
+    }
 
 </style>
