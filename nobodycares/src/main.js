8e5bce47591175500df8b4fa612c1fe7266a84da
CTY <42131250+CTYTC@users.noreply.github.com>
2020-10-13 21:05:14 +0800

    delete import

--- nobodycares/src/main.js
+++ nobodycares/src/main.js
@@ -3,18 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-// Require Froala Editor js file.
-require('froala-editor/js/froala_editor.pkgd.min.js')
-
-// Require Froala Editor css files.
-require('froala-editor/css/froala_editor.pkgd.min.css')
-require('froala-editor/css/froala_style.min.css')
-
-// Import and use Vue Froala lib.
-import VueFroala from 'vue-froala-wysiwyg'
 
 createApp(App)
   .use(store)
   .use(router)
-    .use(VueFroala)
   .mount("#app");
