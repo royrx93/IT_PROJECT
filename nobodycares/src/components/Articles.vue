14a0c01cfce439cf120baf41499589799355feb0
CTY <42131250+CTYTC@users.noreply.github.com>
2020-10-15 14:33:07 +0800

    update article ui

--- nobodycares/src/components/Articles.vue
+++ nobodycares/src/components/Articles.vue
@@ -8,10 +8,10 @@
   <div class = 'border'>
       <div v-for="article in articles" :key="article.title">
         <div class="cardBox">
-            <img style="width: 100%; height: 100%; margin-top: 10px" :src="article.image_url" />
-          <div style="font-size: 5px">{{ article.description }}</div>
-          <router-link :to="{ name: 'ArticleView', params: { title: article.title } }" style=" ">
-            <div style="font-size: 20px; color: whitesmoke; ">{{ article.title }}</div>
+            <img style="width: 100%; height: 100%;" :src="article.image_url" />
+          <div style="font-size: 16px;">{{ article.description }}</div>
+          <router-link :to="{ name: 'ArticleView', params: { title: article.title } }" style="text-decoration: none ">
+            <div style="font-size: 20px; color: whitesmoke; text-decoration: none">{{ article.title }}</div>
           </router-link>
         </div>
       </div>
@@ -76,9 +76,10 @@ export default {
   height: 60px;
   width: 200px;
   font-size: 1.2em;
+    text-decoration: none;
   }
   .border{
-    column-count: 3;
+    column-count: 4;
     margin: 0px auto;
   }
 
@@ -88,8 +89,10 @@ export default {
     width: 90%;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     text-align: center;
+    border-radius: 4px;
     margin: 0px auto;
     margin-top: 10px;
+    margin-bottom: 40px;
   }
 
 </style>
