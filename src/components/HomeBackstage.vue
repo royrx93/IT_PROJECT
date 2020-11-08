76dca038252680eea83bd15c6da695e02942f373
CTY <42131250+CTYTC@users.noreply.github.com>
2020-11-08 12:17:55 +0800

    backstage homepage sidebar

---
+++ src/components/HomeBackstage.vue
@@ -0,0 +1,16 @@
+<template>
+  <sideBar></sideBar>
+</template>
+
+<script>
+import sideBar from "@/components/Sidebar";
+export default {
+  components: {
+    sideBar
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
