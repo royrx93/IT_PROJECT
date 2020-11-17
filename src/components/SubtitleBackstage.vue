84cb81bd03f866b040450c3ff98b61e836ccbc4c
CTY <42131250+CTYTC@users.noreply.github.com>
2020-11-17 14:19:09 +0800

    updatesubtitle

--- src/components/SubtitleBackstage.vue
+++ src/components/SubtitleBackstage.vue
@@ -2,12 +2,18 @@
 <sideBar></sideBar>
   <h1>Subtitle Management</h1>
 
-  <div style="margin-bottom: 40px">
-    <div style="margin-bottom: 10px; font-size: 20px">Update Subtitle</div>
+  <div style="display: inline-block"><button @click="switchToTimeline">Timeline</button></div>
+  <div style="display: inline-block; margin-left: 20px"><button @click="switchToGallery">Gallery</button></div>
+  <div style="display: inline-block; margin-left: 20px"><button @click="switchToArticle">Article</button></div>
+  <div style="display: inline-block; margin-left: 20px"><button @click="switchToContactMe">Contact</button></div>
+
+  <div style="margin-top: 30px; font-size: 30px">Update "{{ display }}" Subtitle</div>
+
+  <div style="margin-top: 30px; margin-bottom: 40px">
     <div style="margin-bottom: 10px">
-      <input id="subtitle" class="title" name="title" v-model="articleSubtitle" />
+      <input id="subtitle" class="title" name="title" v-model="content" />
     </div>
-    <div>
+    <div style="margin-top: 40px">
       <button id="updateSubtitle" @click="updateSubTitle">Submit</button>
     </div>
   </div>
@@ -23,17 +29,44 @@ export default {
   },
   data() {
     return {
-      articleSubtitle: ""
+      content: "",
+      display: ""
     };
   },
   methods: {
     async updateSubTitle() {
-      const response = await ArticleService.updateSubTitle({
-        subtitle: this.articleSubtitle
-      });
-      this.$router.push("/articles");
-      console.log(response);
-    }
+      if(this.content.length > 20){
+        alert("Maximum Length is 15")
+      }else if(this.display == "Article"){
+        const response = await ArticleService.updateSubTitle({
+          subtitle: this.content
+        });
+        this.$router.push("/articles");
+        console.log(response);
+      }else if(this.display == "Timeline"){
+        console.log("t")
+      }else if(this.display == "Gallery"){
+        console.log("g")
+      }else if(this.display == "Contact Me"){
+        console.log("c")
+      }
+    },
+
+    switchToArticle(){
+      this.display = "Article"
+    },
+
+    switchToTimeline(){
+      this.display = "Timeline"
+    },
+
+    switchToGallery(){
+      this.display = "Gallery"
+    },
+
+    switchToContactMe(){
+      this.display = "Contact Me"
+    },
   }
 };
 </script>
@@ -60,6 +93,18 @@ export default {
 
 #subtitle:focus {
   background: whitesmoke;
+}
+
+button {
+  background-color: #33a5ff;
+  width: 60px;
+  height: 25px;
+  color: #ffffff;
+  border: none;
+  border-radius: 10%;
+}
+button:hover {
+  background: #339900;
 }
 
 </style>
