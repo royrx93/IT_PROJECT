cbc701b8a57714e5598b05bb03a46fc15ec367d4
CTY <42131250+CTYTC@users.noreply.github.com>
2020-11-17 21:45:04 +0800

    adjust on margin

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,9 +1,9 @@
 <template>
   <body>
-    <h1>Articles</h1>
+    <div style="margin-top: 60px; font-size: 50px">Articles</div>
 
     <div>
-      <div style="font-size: 30px; margin-bottom: 40px">{{ subtitle }}</div>
+      <div style="font-size: 30px; margin-bottom: 40px; margin-left: 300px"> -- {{ subtitle }}</div>
     </div>
 
     <div class="border">
